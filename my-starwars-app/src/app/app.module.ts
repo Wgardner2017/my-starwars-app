ea22cd654a8ae702f7cff87b14c7296e05ff68b9
Wgardner2017 <30301979+Wgardner2017@users.noreply.github.com>
2018-02-11 20:55:54 -0500

    Revert "functions for components and characters"
    This reverts commit d3d29e37d927dd5229eb76b5e86d6c49a1e3a1e3.

--- my-starwars-app/src/app/app.module.ts
+++ my-starwars-app/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TabsComponent,
-    ListComponent,
-    ItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
